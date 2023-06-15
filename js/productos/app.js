3a360228a020ddc37dc35c30dceada9a413c81b4
migueAngelDev <angeldev1@outlook.com>
2023-06-14 18:43:04 -0600

    funcionalidades agregadas a los filtros de búsqueda en la sección de productos

--- js/productos/app.js
+++ js/productos/app.js
@@ -1,152 +1,124 @@
 const wrapperCards = document.getElementById("wrapperCards");
-const btnGetPrice = document.getElementById("filter-price");
-const filterPriceInicial = document.getElementById("priceInicial");
-const filterPriceFinal = document.getElementById("priceFinal");
+const btnPrice = document.getElementById("filter-price");
+const btnReset = document.getElementById("filter-reset");
+const priceMin = document.getElementById("priceMin");
+const priceMax = document.getElementById("priceMax");
+const inputSearch = document.getElementById("search-item");
+const containerCartShopping = document.querySelector(".counter-items-cart");
+const listItemsProductsCart = document.querySelector(".list-items-products");
 const PRODUCTS = [
-  {
-    nombre: "Aceite para barba",
-    imagen: "./imgs/productos/aceite-barba.jpeg",
-    precio: 300,
-    contador: 0,
-    estado: "activo",
-  },
-  {
-    nombre: "Cera para bigote",
-    imagen: "./imgs/productos/gel-barba.jpeg",
-    precio: 200,
-    contador: 0,
-    estado: "",
-  },
-  {
-    nombre: "Navaja para afeitar",
-    imagen: "./imgs/productos/navaja-afeitar.jpg",
-    precio: 500,
-    contador: 0,
-    estado: "activo",
-  },
-  {
-    nombre: "Tijeras de peluquería",
-    imagen: "./imgs/productos/tijeras-peluqueria.jpg",
-    precio: 600,
-    contador: 0,
-    estado: "",
-  },
-  {
-    nombre: "Peine para barba",
-    imagen: "./imgs/productos/peine-barba.jpg",
-    precio: 80,
-    contador: 0,
-    estado: "activo",
-  },
-  {
-    nombre: "Champú para barba",
-    imagen: "./imgs/productos/champu-barba.jpg",
-    precio: 340,
-    contador: 0,
-    estado: "activo",
-  },
-  {
-    nombre: "Loción después del afeitado",
-    imagen: "./imgs/productos/locion-barba.jpg",
-    precio: 400,
-    contador: 0,
-    estado: "activo",
-  },
-  {
-    nombre: "Gel para peinar",
-    imagen: "./imgs/productos/gel.jpeg",
-    precio: 200,
-    contador: 0,
-    estado: "",
-  },
-  {
-    nombre: "Afeitadora eléctrica",
-    imagen: "./imgs/productos/afeitadora-corte.jpg",
-    precio: 1800,
-    contador: 0,
-    estado: "activo",
-  },
-  {
-    nombre: "Crema para afeitar",
-    imagen: "./imgs/productos/crema-afeitar.jpg",
-    precio: 300,
-    contador: 0,
-    estado: "activo",
-  },
-  {
-    nombre: "Toallas calientes",
-    imagen: "./imgs/productos/toallas-calientes.jpg",
-    precio: 500,
-    contador: 0,
-    estado: "activo",
-  },
-  {
-    nombre: "Gel para barba",
-    imagen: "./imgs/productos/gel-barba.jpeg",
-    precio: 300,
-    contador: 0,
-    estado: "activo",
-  },
-  {
-    nombre: "Maquinilla para afeitar",
-    imagen: "./imgs/productos/maquinilla-afeitar.jpeg",
-    precio: 500,
-    contador: 0,
-    estado: "activo",
-  },
-  {
-    nombre: "Spray de peinado",
-    imagen: "./imgs/productos/spray.jpeg",
-    precio: 240,
-    contador: 0,
-    estado: "activo",
-  },
-  {
-    nombre: "kit de cuidado para la barba",
-    imagen: "./imgs/productos/kit-barba.jpeg",
-    precio: 1000,
-    contador: 0,
-    estado: "activo",
-  },
+	{
+		nombre: "Aceite para barba",
+		imagen: "./imgs/productos/aceite-barba.jpeg",
+		precio: 300,
+		contador: 0,
+	},
+	{
+		nombre: "Cera para bigote",
+		imagen: "./imgs/productos/gel-barba.jpeg",
+		precio: 200,
+		contador: 0,
+	},
+	{
+		nombre: "Navaja para afeitar",
+		imagen: "./imgs/productos/navaja-afeitar.jpg",
+		precio: 500,
+		contador: 0,
+	},
+	{
+		nombre: "Tijeras de peluquería",
+		imagen: "./imgs/productos/tijeras-peluqueria.jpg",
+		precio: 600,
+		contador: 0,
+	},
+	{
+		nombre: "Peine para barba",
+		imagen: "./imgs/productos/peine-barba.jpg",
+		precio: 80,
+		contador: 0,
+	},
+	{
+		nombre: "Champú para barba",
+		imagen: "./imgs/productos/champu-barba.jpg",
+		precio: 340,
+		contador: 0,
+	},
+	{
+		nombre: "Loción después del afeitado",
+		imagen: "./imgs/productos/locion-barba.jpg",
+		precio: 400,
+		contador: 0,
+	},
+	{
+		nombre: "Gel para peinar",
+		imagen: "./imgs/productos/gel.jpeg",
+		precio: 200,
+		contador: 0,
+	},
+	{
+		nombre: "Afeitadora eléctrica",
+		imagen: "./imgs/productos/afeitadora-corte.jpg",
+		precio: 1800,
+		contador: 0,
+	},
+	{
+		nombre: "Crema para afeitar",
+		imagen: "./imgs/productos/crema-afeitar.jpg",
+		precio: 300,
+		contador: 0,
+	},
+	{
+		nombre: "Toallas calientes",
+		imagen: "./imgs/productos/toallas-calientes.jpg",
+		precio: 500,
+		contador: 0,
+	},
+	{
+		nombre: "Gel para barba",
+		imagen: "./imgs/productos/gel-barba.jpeg",
+		precio: 300,
+		contador: 0,
+	},
+	{
+		nombre: "Maquinilla para afeitar",
+		imagen: "./imgs/productos/maquinilla-afeitar.jpeg",
+		precio: 500,
+		contador: 0,
+	},
+	{
+		nombre: "Spray de peinado",
+		imagen: "./imgs/productos/spray.jpeg",
+		precio: 240,
+		contador: 0,
+	},
+	{
+		nombre: "kit de cuidado para la barba",
+		imagen: "./imgs/productos/kit-barba.jpeg",
+		precio: 1000,
+		contador: 0,
+	},
 ];
 
-const rangePrice = (priceMin, priceMax, precio, preview) => {
-  if (precio >= priceMin && priceMax === "") {
-    console.log(preview);
-    wrapperCards.innerHTML = "";
-    wrapperCards.innerHTML += preview;
-  } else if (precio >= priceMin && precio <= priceMax) {
-    console.log(preview);
-    wrapperCards.innerHTML = "";
-    wrapperCards.innerHTML += preview;
-  }
-  return wrapperCards;
-};
-
-PRODUCTS.forEach((producto, index) => {
-  const RENDERING = `
-	<li class="card">
+function renderingProducts(array) {
+	let rendering = "";
+	array.forEach((producto, index) => {
+		rendering += `
+	<li class="card" id="${index}">
 		<section class="sectionImg">
 			<img src="${producto.imagen}" alt="${producto.nombre}" class="card-img" >
 		</section>
 		<section class="section-padding">
 			<div class="content-title">
-				<p class="title-card">
-					${producto.nombre}
-				</p>
+				<p class="title-card">${producto.nombre}</p>
 			</div>
 			<div class="contentPriceCard">
-				<p class="priceCard">
-					$${producto.precio.toFixed(2)}
-				</p>
+				<p class="priceCard">$${producto.precio.toFixed(2)}</p>
 			</div>
 			<div class="section-counter">
 				<button class="remove-product" data-index="${index}">
 					-
 				</button>
-				<p class="counter-product" data-index="${index}">
-					${producto.contador}
-				</p>
+				<p class="counter-product" data-index="${index}">${producto.contador}</p>
 				<button class="add-product" data-index="${index}">
 					+
 				</button>
@@ -159,87 +131,150 @@ PRODUCTS.forEach((producto, index) => {
 		</section>
 	</li>
 	`;
-  wrapperCards.innerHTML += RENDERING;
 
-  btnGetPrice.addEventListener("click", () => {
-    let priceInicial = filterPriceInicial.value;
-    let priceFinal = filterPriceFinal.value;
-    rangePrice(priceInicial, priceFinal, producto.precio, RENDERING);
-  });
+		wrapperCards.innerHTML = rendering;
+	});
+
+	const removeProduct = document.querySelectorAll(".remove-product");
+	const addProduct = document.querySelectorAll(".add-product");
+	const addProductCart = document.querySelectorAll(
+		".btn-add-to-shopping-cart"
+	);
+
+	removeProduct.forEach((btnRemove) => {
+		btnRemove.addEventListener(
+			"click",
+			({
+				target: {
+					dataset: { index },
+				},
+			}) => {
+				const counterElement = document.querySelector(
+					`.counter-product[data-index="${index}"]`
+				);
+				const product = PRODUCTS[index];
+				if (product.contador > 0) {
+					product.contador--;
+				}
+				counterElement.textContent = product.contador;
+			}
+		);
+	});
+
+	addProduct.forEach((btnAdd) => {
+		btnAdd.addEventListener(
+			"click",
+			({
+				target: {
+					dataset: { index },
+				},
+			}) => {
+				const counterElement = document.querySelector(
+					`.counter-product[data-index="${index}"]`
+				);
+				const product = PRODUCTS[index];
+				product.contador++;
+				counterElement.textContent = product.contador;
+			}
+		);
+	});
+
+	addProductCart.forEach((addCart) => {
+		addCart.addEventListener(
+			"click",
+			({
+				target: {
+					dataset: { index },
+				},
+			}) => {
+				const idItemProduct = document.getElementById(`${index}`);
+				const titleItemProduct =
+					idItemProduct.querySelector(".title-card");
+				const priceItemProduct =
+					idItemProduct.querySelector(".priceCard");
+				let itemsProduct =
+					idItemProduct.querySelector(".counter-product");
+
+				if (itemsProduct.textContent > 0) {
+					console.log(idItemProduct);
+					console.log(titleItemProduct.textContent);
+					console.log(priceItemProduct.textContent);
+					console.log(itemsProduct.textContent);
+					itemsProduct.textContent = 0;
+					PRODUCTS[index].contador = 0;
+					console.log(PRODUCTS[index]);
+				}
+			}
+		);
+	});
+}
+
+function notFoundItem() {
+	wrapperCards.innerHTML = "";
+	return (wrapperCards.innerHTML = `
+				<section class="not-found-item">
+							<p>!Ups, no se ha encontrado ninguna coincidencia</p>
+				</section>
+	`);
+}
+
+function filterPriceProducts() {
+	const minPrice = parseInt(priceMin.value);
+	const maxPrice = parseInt(priceMax.value);
+	const filteredProducts = PRODUCTS.filter((product) => {
+		if (product.precio >= minPrice && product.precio <= maxPrice) {
+			return product;
+		}
+
+		if (product.precio >= minPrice && !maxPrice) {
+			return product;
+		}
+
+		if (product.precio <= maxPrice && !minPrice) {
+			return product;
+		}
+
+		if (
+			product.precio >= minPrice !== product.precio &&
+			product.precio <= maxPrice !== product.precio
+		) {
+			notFoundItem();
+		}
+	});
+
+	renderingProducts(filteredProducts);
+}
+
+function resetProducts() {
+	priceMax.value = "";
+	priceMin.value = "";
+	renderingProducts(PRODUCTS);
+}
+
+btnPrice.addEventListener("click", (e) => {
+	e.preventDefault();
+	filterPriceProducts();
 });
 
-const removeProduct = document.querySelectorAll(".remove-product");
-const addProduct = document.querySelectorAll(".add-product");
-const addProductCart = document.querySelectorAll(".btn-add-to-shopping-cart");
-
-removeProduct.forEach((btnRemove) => {
-  btnRemove.addEventListener(
-    "click",
-    ({
-      target: {
-        dataset: { index },
-      },
-    }) => {
-      const counterElement = document.querySelector(
-        `.counter-product[data-index="${index}"]`
-      );
-      const product = PRODUCTS[index];
-      if (product.contador > 0) {
-        product.contador--;
-      }
-      counterElement.textContent = product.contador;
-    }
-  );
+btnReset.addEventListener("click", (e) => {
+	e.preventDefault();
+	resetProducts();
 });
 
-addProduct.forEach((btnAdd) => {
-  btnAdd.addEventListener(
-    "click",
-    ({
-      target: {
-        dataset: { index },
-      },
-    }) => {
-      const counterElement = document.querySelector(
-        `.counter-product[data-index="${index}"]`
-      );
-      const product = PRODUCTS[index];
-      product.contador++;
-      counterElement.textContent = product.contador;
-    }
-  );
+inputSearch.addEventListener("keyup", ({ target: { value } }) => {
+	const resultSearch = PRODUCTS.filter((product) => {
+		let itemFound = product.nombre
+			.toLocaleLowerCase()
+			.includes(value.toLocaleLowerCase());
+
+		if (itemFound) {
+			return product;
+		} else {
+			notFoundItem();
+		}
+	});
+
+	renderingProducts(resultSearch);
 });
 
-addProductCart.forEach((addCart) => {
-  addCart.addEventListener(
-    "click",
-    ({
-      target: {
-        dataset: { index },
-      },
-    }) => {
-      const counterItem = PRODUCTS[index].contador;
-      if (counterItem > 0) {
-        const msg = document.querySelector(
-          `.section-add-item-cart[data-index="${index}"]`
-        );
-        msg.innerHTML = "";
-        msg.innerHTML += `
-				<p class="msg-success" data-index="${index}">Se ha agregado el producto correctamente</p>
-				`;
-        const msgSuccess = document.querySelector(
-          `.msg-success[data-index="${index}"]`
-        );
-        setTimeout(() => {
-          msg.innerHTML = "";
-          msg.appendChild(addCart);
-          PRODUCTS[index].contador = 0;
-          const counterElement = document.querySelector(
-            `.counter-product[data-index="${index}"]`
-          );
-          counterElement.textContent = 0;
-        }, 2500);
-      }
-    }
-  );
-});
+renderingProducts(PRODUCTS);
