2a29b2b5ef81cf533c19c9f97013e5b95a36879b
TanniaIsabelDev <wingshara7@gmail.com>
2023-06-19 16:58:59 -0600

    Menú Responsivo

--- js/home/home.js
+++ js/home/home.js
@@ -1,55 +1,6 @@
 import { getOpinones } from "../formulario/firebase.js";
 const containerOpinions = document.getElementById("container-testimonials");
 
-/* Funcionalidad del menu */
-
-(function () {
-  const listElements = document.querySelectorAll(".menu-wrapper--show");
-  const list = document.querySelector(".menu-content");
-  const menu = document.querySelector(".menu__hamburguer");
-
-  const addClick = () => {
-    listElements.forEach((element) => {
-      element.addEventListener("click", () => {
-        let subMenu = element.children[1];
-        let height = 0;
-
-        if (subMenu.clientHeight === 0) {
-          height = subMenu.scrollHeight;
-        }
-
-        subMenu.style.height = `${height}px`;
-      });
-    });
-  };
-
-  const deleteStyleHeight = () => {
-    listElements.forEach((element) => {
-      if (element.children[1].getAttribute("style")) {
-        element.children[1].removeAttribute("style");
-      }
-    });
-  };
-
-  window.addEventListener("resize", () => {
-    if (window.innerWidth > 800) {
-      deleteStyleHeight();
-      if (list.classList.contains("menu-content--show"))
-        list.classList.remove("menu-content--show");
-    } else {
-      addClick();
-    }
-  });
-
-  if (window.innerWidth <= 800) {
-    addClick();
-  }
-
-  menu.addEventListener("click", () =>
-    list.classList.toggle("menu-content--show")
-  );
-})();
-
 /* Funcionalidad de las estrellas del footer */
 
 const starLinks = document.querySelectorAll(".star-link");
